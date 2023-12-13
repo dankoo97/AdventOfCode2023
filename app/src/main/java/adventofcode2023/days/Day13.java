fc4424e9b32d1beb719d90c0b4453e4274b2a8ec
dankoo97 <dak.7997@gmail.com>
2023-12-12 23:35:21 -0500

    Created day 13

---
+++ app/src/main/java/adventofcode2023/days/Day13.java
@@ -0,0 +1,20 @@
+package adventofcode2023.days;
+
+import java.io.InputStream;
+
+public class Day13 extends AoCDay {
+
+    public Day13(InputStream file) {
+        super(file);
+    }
+
+    @Override
+    public Object runPart1() {
+        return null;
+    }
+
+    @Override
+    public Object runPart2() {
+        return null;
+    }
+}
